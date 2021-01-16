a38b0c135ae482d332b8e05659a2c58b68bf8f46
LukashevV <valikallstars@gmail.com>
2021-01-16 19:52:31 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -17,50 +17,50 @@ window.addEventListener('DOMContentLoaded', () => {
     // });
     // Animations
 
-    // const animItems = document.querySelectorAll('._anim-items');
+    const animItems = document.querySelectorAll('._anim-items');
 
-    // if (animItems.length > 0) {
+    if (animItems.length > 0) {
 
-    //     window.addEventListener('scroll', animOnScroll);
+        window.addEventListener('scroll', animOnScroll);
 
-    //     function animOnScroll() {
-    //         for (let index = 0; index < animItems.length; index++) {
-    //             const animItem = animItems[index];
-    //             const animItemHeight = animItem.offsetHeight;
-    //             const animItemOffset = offset(animItem).top;
-    //             const animStart = 3;
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 3;
 
-    //             let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-    //             if (animItemHeight > window.innerHeight) {
-    //                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-    //             }
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
 
-    //             if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-    //                 animItem.classList.add('_active');
-    //             } else {
-    //                 if (!animItem.classList.contains('_anim-off')) {
-    //                     animItem.classList.remove('_active');
-    //                 }
-    //             }
-    //         }
-    //     }
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-off')) {
+                        animItem.classList.remove('_active');
+                    }
+                }
+            }
+        }
 
-    //     function offset(el) {
-    //         const rect = el.getBoundingClientRect(),
-    //             scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    //             scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //         return {
-    //             top: rect.top + scrollTop,
-    //             left: rect.left + scrollLeft
-    //         }
-    //     }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {
+                top: rect.top + scrollTop,
+                left: rect.left + scrollLeft
+            }
+        }
 
-    //     setTimeout(() => {
-    //         animOnScroll();
-    //     }, 300);
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
 
-    // }
+    }
     /* Jquery */
     $(document).ready(function () {
         // navmenu on scroll
@@ -82,9 +82,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
         // gotop
 
-        // $('.gotop').click(function () {
-        //     scroll(0, 0)
-        // });
+        $('.gotop').click(function () {
+            scroll(0, 0)
+        });
 
         // mailer
         // $('form').submit(function (e) {
